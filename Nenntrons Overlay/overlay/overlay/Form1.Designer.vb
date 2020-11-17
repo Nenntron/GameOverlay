5642b2707020256b90bcc8ebcf2d048e00b4f083
Nenntron <74608238+Nenntron@users.noreply.github.com>
2020-11-17 15:37:53 +0100

    Add files via upload

---
+++ Nenntrons Overlay/overlay/overlay/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(794, 443)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(41, 22)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(226, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.AutoSize = False
+        Me.TrackBar1.BackColor = System.Drawing.Color.White
+        Me.TrackBar1.Location = New System.Drawing.Point(130, 9)
+        Me.TrackBar1.Maximum = 100
+        Me.TrackBar1.Minimum = 5
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(92, 22)
+        Me.TrackBar1.TabIndex = 2
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.None
+        Me.TrackBar1.Value = 100
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.Color.White
+        Me.CheckBox1.Location = New System.Drawing.Point(59, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(68, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "TopMost"
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Font = New System.Drawing.Font("Consolas", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(104, 200)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(575, 37)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Drag and Drop any Image onto Me"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(673, 13)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Nenntrons Overlay Tool"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Aqua
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Nenntrons Overlay/overlay/overlay/Form1.vb
@@ -0,0 +1,178 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        PictureBox1.AllowDrop = True
+
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.DoubleBuffered = True
+        Me.SetStyle(ControlStyles.ResizeRedraw, True)
+        Label1.Text = "Opacity"
+
+        TopMost = True
+
+    End Sub
+
+    Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+        e.Graphics.FillRectangle(Brushes.Green, Top)
+        e.Graphics.FillRectangle(Brushes.Green, Left)
+        e.Graphics.FillRectangle(Brushes.Green, Right)
+        e.Graphics.FillRectangle(Brushes.Green, Bottom)
+    End Sub
+
+    Private Const HTLEFT As Integer = 10, HTRIGHT As Integer = 11, HTTOP As Integer = 12, HTTOPLEFT As Integer = 13, HTTOPRIGHT As Integer = 14, HTBOTTOM As Integer = 15, HTBOTTOMLEFT As Integer = 16, HTBOTTOMRIGHT As Integer = 17
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        MyBase.WndProc(m)
+        If m.Msg = &H84 Then
+            Dim mp = Me.PointToClient(Cursor.Position)
+
+            If TopLeft.Contains(mp) Then
+                m.Result = CType(HTTOPLEFT, IntPtr)
+            ElseIf TopRight.Contains(mp) Then
+                m.Result = CType(HTTOPRIGHT, IntPtr)
+            ElseIf BottomLeft.Contains(mp) Then
+                m.Result = CType(HTBOTTOMLEFT, IntPtr)
+            ElseIf BottomRight.Contains(mp) Then
+                m.Result = CType(HTBOTTOMRIGHT, IntPtr)
+            ElseIf Top.Contains(mp) Then
+                m.Result = CType(HTTOP, IntPtr)
+            ElseIf Left.Contains(mp) Then
+                m.Result = CType(HTLEFT, IntPtr)
+            ElseIf Right.Contains(mp) Then
+                m.Result = CType(HTRIGHT, IntPtr)
+            ElseIf Bottom.Contains(mp) Then
+                m.Result = CType(HTBOTTOM, IntPtr)
+            End If
+        End If
+    End Sub
+
+    Dim rng As New Random
+    Function randomColour() As Color
+        Return Color.FromArgb(255, rng.Next(255), rng.Next(255), rng.Next(255))
+    End Function
+
+    Const ImaginaryBorderSize As Integer = 16
+
+    Function Top() As Rectangle
+        Return New Rectangle(0, 0, Me.ClientSize.Width, ImaginaryBorderSize)
+    End Function
+
+    Function Left() As Rectangle
+        Return New Rectangle(0, 0, ImaginaryBorderSize, Me.ClientSize.Height)
+    End Function
+
+    Function Bottom() As Rectangle
+        Return New Rectangle(0, Me.ClientSize.Height - ImaginaryBorderSize, Me.ClientSize.Width, ImaginaryBorderSize)
+    End Function
+
+    Function Right() As Rectangle
+        Return New Rectangle(Me.ClientSize.Width - ImaginaryBorderSize, 0, ImaginaryBorderSize, Me.ClientSize.Height)
+    End Function
+
+    Function TopLeft() As Rectangle
+        Return New Rectangle(0, 0, ImaginaryBorderSize, ImaginaryBorderSize)
+    End Function
+
+    Function TopRight() As Rectangle
+        Return New Rectangle(Me.ClientSize.Width - ImaginaryBorderSize, 0, ImaginaryBorderSize, ImaginaryBorderSize)
+    End Function
+
+    Function BottomLeft() As Rectangle
+        Return New Rectangle(0, Me.ClientSize.Height - ImaginaryBorderSize, ImaginaryBorderSize, ImaginaryBorderSize)
+    End Function
+
+    Function BottomRight() As Rectangle
+        Return New Rectangle(Me.ClientSize.Width - ImaginaryBorderSize, Me.ClientSize.Height - ImaginaryBorderSize, ImaginaryBorderSize, ImaginaryBorderSize)
+    End Function
+
+    Public Const WM_NCLBUTTONDOWN As Integer = 161  'Detect if left mouse is pressed down
+    Public Const HT_CAPTION As Integer = 2
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Me.Opacity = TrackBar1.Value / 100
+        Label1.Text = TrackBar1.Value.ToString + "%"
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Me.TopMost = True
+        Else
+            Me.TopMost = False
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim result3 As DialogResult = MessageBox.Show("If you found this tool helpful" & vbCrLf & "please consider to Follow me or Like my Thread", "You are asked!", MessageBoxButtons.YesNo)
+
+        ' Test DialogResult.
+        If result3 = DialogResult.Yes Then
+            Process.Start("https://forums.mihoyo.com/genshin/accountCenter/postList?id=87317176")
+        End If
+
+
+    End Sub
+
+    <DllImport("User32")> Private Shared Function SendMessage(hWnd As IntPtr, Msg As Integer, wParam As Integer, lParam As Integer) As Integer  'DLLImport for Moveable Window without Border
+    End Function
+    <DllImport("User32")> Private Shared Function ReleaseCapture() As Boolean   'DLLImport for Moveable Window without Border
+    End Function
+
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown    'Be able to move the window with left mouse although it is borderless
+        If (e.Button = MouseButtons.Left) Then
+            ReleaseCapture()
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0)
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        If (e.Button = MouseButtons.Left) Then
+            ReleaseCapture()
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0)
+        End If
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        AddHandler PictureBox1.DragDrop, AddressOf PictureBox1_DragDrop
+        AddHandler PictureBox1.DragEnter, AddressOf PictureBox1_DragEnter
+
+    End Sub
+
+    Private Sub PictureBox1_DragDrop(sender As Object, e As DragEventArgs) Handles PictureBox1.DragDrop
+
+        Dim picbox As PictureBox = CType(sender, PictureBox)
+
+        Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+        If files.Length <> 0 Then
+            Try
+                picbox.BackgroundImage = Image.FromFile(files(0))
+            Catch ex As Exception
+                MessageBox.Show("Problem opening file ")
+            End Try
+        End If
+    End Sub
+
+    Private Sub PictureBox1_DragEnter(sender As Object, e As DragEventArgs) Handles PictureBox1.DragEnter
+
+        Label2.Hide()
+
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+End Class
